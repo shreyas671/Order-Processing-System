386068ae811afbffc1296e856717abd302d049da
Shreyas Rakesh Jaiswal <shreyasjaiswal@Shreyass-MacBook-Air.local>
2026-02-18 20:44:38 -0500

    Committing Episode 16 Changes (Kafka Implementation)

--- order_service/src/express-app.ts
+++ order_service/src/express-app.ts
@@ -2,20 +2,42 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cartRoutes from "./routes/cart.routes";
 import orderRoutes from "./routes/order.routes";
-import { httpLogger, HandleErrorWithLogger } from "./utils";
+import { httpLogger, HandleErrorWithLogger, MessageBroker } from "./utils";
+import { Consumer, Producer } from "kafkajs";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(httpLogger);
+export const ExpressApp = async() => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(httpLogger);
 
-app.use(cartRoutes);
-app.use(orderRoutes);
+  const producer = await MessageBroker.connectProducer<Producer>();
+  producer.on("producer.connect", () => {
+    console.log("Producer Connected");
+  })
 
-app.use("/", (req: Request, res: Response, _: NextFunction) => {
-  return res.status(200).json({ message: "I am healthy!" });
-});
+  const consumer = await MessageBroker.connectConsumer<Consumer>();
+  consumer.on("consumer.connect", () => {
+    console.log("Consumer Connected");
+  })
 
-app.use(HandleErrorWithLogger);
 
-export default app;
+  //Step 2: Subscribe to topic or publish the message 
+
+  await MessageBroker.subscribe((message) => {
+    console.log("Consumer Received the Message");
+    console.log("Message received", message);
+  }, "OrderEvents");
+
+  app.use(cartRoutes);
+  app.use(orderRoutes);
+
+  app.use("/", (req: Request, res: Response, _: NextFunction) => {
+    return res.status(200).json({ message: "I am healthy!" });
+  });
+
+  app.use(HandleErrorWithLogger);
+
+  return app;
+}
+
--- order_service/src/routes/order.routes.ts
+++ order_service/src/routes/order.routes.ts
@@ -1,10 +1,35 @@
 import express, { NextFunction, Request, Response } from "express";
+import { MessageBroker } from "../utils";
+import { OrderEvent } from "../types";
 
 const router = express.Router();
 
 router.post(
   "/order",
   async (req: Request, res: Response, next: NextFunction) => {
+
+    //Order Create Logic
+
+    // Step 3: Publish the Message
+    await MessageBroker.publish({
+      topic: "OrderEvents",
+      headers: { token: req.headers.authorization },
+      event: OrderEvent.CREATE_ORDER,
+      message: {
+        OrderId: 1,
+        items: [
+          {
+            productId: 1,
+            quantity: 1
+          },
+          {
+            productId: 2,
+            quantity: 2
+          }
+        ],
+      },
+    });
+
     return res.status(200).json({ message: "create order" });
   }
 );
